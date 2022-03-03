0ce2cdf6559d4ef6b235e8fd18458c79c5931de5
Anowar <1hafizajr@gmail.com>
2022-03-03 23:19:10 +0600

    add middelware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,8 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
+        'authadmin' => \App\Http\Middleware\AuthAdmin::class,
+        'serviceprovider' => \App\Http\Middleware\ServiceProvider::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -153,7 +153,9 @@
 @csrf
 </form>
  @else
- <li class="login-form"><a href="{{route('register')}}" title="Register">Register</a></li>
+ <li class="login-form">
+     <a href="{{route('register')}}" title="Register">Register</a>
+</li>
  <li class="login-form"><a href="{{route('login')}}" title="Login">Login</a></li>
 @endif
 @endif
