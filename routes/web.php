8d2e49d0ba5949daf32af06f1dbb0d12c7dc99a6
Anowar <1hafizajr@gmail.com>
2022-03-03 21:50:01 +0600

    add jetstream

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 
  route::get('/', HomeComponent::class)->name('home');
  
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
