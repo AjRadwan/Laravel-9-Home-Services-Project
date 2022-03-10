7e2fd61d902783af0cafeaee12dddb6769bc713c
Anowar <1hafizajr@gmail.com>
2022-03-10 17:59:28 +0600

    add edit component

---
+++ app/Http/Livewire/Admin/AdminEditServiceCatComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class AdminEditServiceCatComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.admin-edit-service-cat-component');
+    }
+}
