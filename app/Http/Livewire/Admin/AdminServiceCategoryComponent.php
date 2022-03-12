2e00414b97d1f465ed8bcfd1ef16a5407cfa0fd4
Anowar <1hafizajr@gmail.com>
2022-03-12 21:23:51 +0600

    add table

--- app/Http/Livewire/Admin/AdminServiceCategoryComponent.php
+++ app/Http/Livewire/Admin/AdminServiceCategoryComponent.php
@@ -1,9 +1,7 @@
 <?php
 
 namespace App\Http\Livewire\Admin;
- 
 use Livewire\Component;
- 
 use App\Models\ServiceCategory;
 class AdminServiceCategoryComponent extends Component{   
 
@@ -18,7 +16,7 @@ class AdminServiceCategoryComponent extends Component{
     }
    
     public function render(){
-         $categories = ServiceCategory::paginate(5);
+         $categories = ServiceCategory::paginate(8);
         return view('livewire.admin.admin-service-category-component', compact('categories'))->layout('layouts.base');
     }
 }
